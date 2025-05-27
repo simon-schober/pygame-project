13cfef6cca29d979d9616aa75638621b6d47b0f2
simon <simon.schober@tutanota.com>
2025-05-27 15:08:03 +0200

    First understanding of openGL and seperate function for graphics initialization, 2h

---
+++ src/util.py
@@ -0,0 +1,24 @@
+import sys, pygame
+from pygame.locals import *
+from pygame.constants import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+def init_graphics ():
+    pygame.init()
+    viewport = (800,600)
+    srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
+
+    glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_COLOR_MATERIAL)
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)           # most obj files expect to be smooth-shaded
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    width, height = viewport
+    gluPerspective(90.0, width/float(height), 1, 100.0)
